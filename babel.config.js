12e43cbb7793b8100898bc0e9387e944b3922edb
Jerome Fitzgerald <jeromes@gmail.com>
2019-01-09 18:44:55 -0500

    🚧️ mjs: code-splitting #4
    Can't get it to import properly via `.mjs`
     but `.jsx` works fine. (Not sure if it splits.)
    
    I'm probably going to stand-down on this, I need
     to learn more about `.mjs` and how we'd need
     to transpile (if at all).
    
    📝️ NOTE: I didn't test `.jsx` rename without
     the `esm` plugins running so maybe not needed.
    
    📦️ packages:
    - @babel/plugin-syntax-dynamic-import@7.2.0
    - @babel/plugin-syntax-jsx@7.2.0
    - @babel/plugin-transform-react-inline-elements@7.2.0
    - @babel/plugin-transform-react-jsx@7.2.0
    - cross-env@5.2.0
    - renamer@1.1.1

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,29 @@
-module.exports = {
-  presets: [['next/babel'], 'minify'],
-  plugins: ['@babel/plugin-transform-react-jsx'],
+// eslint-disable-next-line func-names
+module.exports = function(api) {
+  const env = api.env();
+  api.cache(true);
+
+  const config = {};
+  config.presets = [];
+  config.plugins = [];
+
+  if (['cjs'].indexOf(env) > -1) {
+    config.presets.push([
+      'next/babel',
+      { 'preset-env': { modules: 'commonjs' } },
+    ]);
+    config.presets.push(['minify']);
+    config.plugins.push(['@babel/plugin-transform-react-jsx']);
+  }
+
+  if (['esm'].indexOf(env) > -1) {
+    config.presets.push(['next/babel', { 'preset-env': { modules: false } }]);
+    // config.plugins.push(['@babel/plugin-syntax-dynamic-import']);
+    // config.plugins.push(['@babel/plugin-syntax-jsx']);
+    // config.plugins.push(['@babel/plugin-transform-react-inline-elements']);
+    // config.plugins.push(['@babel/plugin-transform-react-jsx']);
+    config.presets.push(['minify']);
+  }
+
+  return config;
 };
